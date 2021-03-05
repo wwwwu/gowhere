38803abf413fe3a620a3d96c10b49c3995f506f5
wmeng <280990704@qq.com>
2021-03-05 14:49:07 +0800

    Add files via upload

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class='banner' @click='handleBannerClick'>
+            <img class='banner-img' :src='bannerImg' />
+            <div class='banner-info'>
+                <div class='banner-title'>{{this.sightName}}</div>
+                <div class='banner-number'><span class="iconfont banner-icon">&#xe6ca;</span>{{this.bannerImgs.length}}</div>
+            </div>
+        </div>
+      <fade-animation>
+        <common-gallary :imgs='bannerImgs' v-show="showGallary" 
+                      @close='handleGallaryClose'></common-gallary>
+      </fade-animation>
+      
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
+export default {
+    name: 'DetailBanner',
+    props: {
+      sightName: String,
+      bannerImg: String,
+      bannerImgs: Array
+    },
+    data() {
+      return {
+        showGallary: false,
+        imgs: ['https://imgs.qunarzz.com/p/p62/1411/ff/ffa95ccd8a747e23213a9cdb.jpg_256x160_d563a0e8.jpg',
+                'https://imgs.qunarzz.com/p/p45/1809/22/d815539e74dc4a02.jpg_256x160_00a6f984.jpg']
+      }
+    },
+    methods: {
+      handleBannerClick () {
+        this.showGallary = true
+      },
+      handleGallaryClose () {
+        this.showGallary = false
+      }
+    },
+    components : {
+      CommonGallary,
+      FadeAnimation
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner
+      position:relative
+      overflow: hidden
+      height: 0
+      padding-bottom: 55%
+      .banner-img
+        width: 100%
+      .banner-info
+        display: flex
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        line-height: .6rem
+        color: #fff
+        background-image:linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+        .banner-title
+          font-size: .32rem
+          padding: 0 .2rem
+          flex: 1
+        .banner-number
+          margin-top: .14rem
+          padding: 0 .4rem
+          height: .32rem
+          line-height: .32rem
+          border-radius: .2rem
+          background: rgba(0, 0, 0, .8)
+          font-size: .24rem
+          .banner-icon
+            font-size: .24rem
+            margin-right: .1rem
+</style>
